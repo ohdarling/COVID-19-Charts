6f2311beaa0f190b048235c52becb9c51c74b630
ohdarling88 <ohdarling88@gmail.com>
2020-02-05 08:54:37 +0800

    update

---
+++ build.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const fs = require('fs');
+
+const download = function(url, dest, cb) {
+  const file = fs.createWriteStream(dest);
+  https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
+
+download('https://github.com/BlankerL/DXY-2019-nCoV-Data/raw/master/DXYArea.csv', 'public/DXYArea.csv');
--- docs/deploy.sh
+++
@@ -1,4 +0,0 @@
-rm -rf DXY-2019-nCoV-Data
-git clone https://github.com/BlankerL/DXY-2019-nCoV-Data.git --depth=1
-mv DXY-2019-nCoV-Data/DXYArea.csv ./
-rm -rf DXY-2019-nCoV-Data
