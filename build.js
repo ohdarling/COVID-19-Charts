34033dc983a5cfbdcef8ce55055e190bd189894a
ohdarling88 <ohdarling88@gmail.com>
2020-02-07 00:12:14 +0800

    parse data at building

--- build.js
+++ build.js
@@ -1,5 +1,21 @@
 const https = require('https');
 const fs = require('fs');
+const { parseData, processDuplicatedData, toSortedProvinceData, calcIncreasement } = require('./parse_data');
+
+function generateConfigs() {
+  const csvData = fs.readFileSync('DXYArea.csv', { encoding: 'utf8' });
+  console.log('Parsing...');
+  let provsData = parseData(csvData);
+  console.log('Processing duplicated data...');
+  provsData = processDuplicatedData(provsData);
+  console.log('Sorting by confirmed count...');
+  provsData = toSortedProvinceData(provsData);
+  console.log('Calculating increasement...');
+  provsData = calcIncreasement(provsData);
+  const dataStr = JSON.stringify(provsData);
+  fs.writeFileSync('public/data.json', dataStr);
+  console.log('Done.');
+}
 
 const download = function(url, dest) {
   const file = fs.createWriteStream(dest);
@@ -8,6 +24,7 @@ const download = function(url, dest) {
     file.on('finish', function() {
       file.close();  // close() is async, call cb after close completes.
       console.log('get success');
+      generateConfigs();
       process.exit(0);
     });
   }).on('error', function(err) { // Handle errors
@@ -17,5 +34,5 @@ const download = function(url, dest) {
   });
 };
 
-download('https://raw.githubusercontent.com/BlankerL/DXY-2019-nCoV-Data/master/csv/DXYArea.csv', 'public/DXYArea.csv');
+download('https://raw.githubusercontent.com/BlankerL/DXY-2019-nCoV-Data/master/csv/DXYArea.csv', 'DXYArea.csv');
 setTimeout(() => {}, 0);
