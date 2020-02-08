487bd611748ed5394568788fa60956546293140e
ohdarling88 <ohdarling88@gmail.com>
2020-02-08 20:51:20 +0800

    update

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const { parseData, processDuplicatedData, toSortedProvinceData, calcIncreasement } = require('./parse_data');
-const dataFileName = 'tmp.csv';
+const dataFileName = 'DXYArea.csv';
 
 function generateConfigs() {
   const csvData = fs.readFileSync(dataFileName, { encoding: 'utf8' });
@@ -20,27 +20,28 @@ function generateConfigs() {
   fs.unlinkSync(dataFileName);
 }
 
-const download = function(url, dest) {
-  if (fs.existsSync(dest)) {
-    fs.unlinkSync(dest);
-  }
-  const file = fs.createWriteStream(dest);
-  https.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close();  // close() is async, call cb after close completes.
-      console.log('get success');
-    });
-    file.on('close', () => {
-      generateConfigs();
-      process.exit(0);
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest, () => {}); // Delete the file async. (But we don't check the result)
-    console.log('get failed', err);
-    process.exit(1);
-  });
-};
+generateConfigs();
 
-download('https://raw.githubusercontent.com/BlankerL/DXY-2019-nCoV-Data/master/csv/DXYArea.csv', dataFileName);
-setTimeout(() => {}, 0);
+// const download = function(url, dest) {
+//   if (fs.existsSync(dest)) {
+//     fs.unlinkSync(dest);
+//   }
+//   const file = fs.createWriteStream(dest);
+//   http.get(url, function(response) {
+//     response.pipe(file);
+//     file.on('finish', function() {
+//       file.close();  // close() is async, call cb after close completes.
+//       console.log('get success');
+//     });
+//     file.on('close', () => {
+//       generateConfigs();
+//       process.exit(0);
+//     });
+//   }).on('error', function(err) { // Handle errors
+//     fs.unlink(dest, () => {}); // Delete the file async. (But we don't check the result)
+//     console.log('get failed', err);
+//     process.exit(1);
+//   });
+// };
+
+// download('https://raw.githubusercontent.com/BlankerL/DXY-2019-nCoV-Data/master/csv/DXYArea.csv', dataFileName);
