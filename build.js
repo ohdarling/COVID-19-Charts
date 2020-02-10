ff282d390e9f0137280da2203030a425d8650aba
ohdarling88 <ohdarling88@gmail.com>
2020-02-10 18:36:48 +0800

    clean

--- build.js
+++ build.js
@@ -1,5 +1,3 @@
-const https = require('https');
-const http = require('http');
 const fs = require('fs');
 const { generateFromCSV, toDateSeriesData } = require('./parse_data');
 const dataFileName = 'DXYArea.csv';
@@ -18,27 +16,3 @@ function generateConfigs() {
 }
 
 generateConfigs();
-
-// const download = function(url, dest) {
-//   if (fs.existsSync(dest)) {
-//     fs.unlinkSync(dest);
-//   }
-//   const file = fs.createWriteStream(dest);
-//   http.get(url, function(response) {
-//     response.pipe(file);
-//     file.on('finish', function() {
-//       file.close();  // close() is async, call cb after close completes.
-//       console.log('get success');
-//     });
-//     file.on('close', () => {
-//       generateConfigs();
-//       process.exit(0);
-//     });
-//   }).on('error', function(err) { // Handle errors
-//     fs.unlink(dest, () => {}); // Delete the file async. (But we don't check the result)
-//     console.log('get failed', err);
-//     process.exit(1);
-//   });
-// };
-
-// download('https://raw.githubusercontent.com/BlankerL/DXY-2019-nCoV-Data/master/csv/DXYArea.csv', dataFileName);
